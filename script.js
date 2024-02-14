a57ad1b416f6e580251aae4c70eb1c6b1a69902b
Rasc3ta <greenfair.shan5@gmail.com>
2024-02-14 12:32:00 +0600

    triangle area function done

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('shit')
+const getValues = (Event) => {
+    const inputs = Event.target.parentElement.getElementsByTagName('input');
+
+    const value1 = parseFloat(inputs[0].value);
+    const value2 = parseFloat(inputs[1].value);
+
+    return [value1, value2]
+} 
+
+
+const triangleArea = (Event) => {
+    const valueArray = getValues(Event);
+    
+    const area = .5 * valueArray[0] * valueArray[1];
+    console.log(area);
+}
+const triangle = document.getElementById('triangle');
+
+triangle.querySelector('button').addEventListener('click', triangleArea);
